c1eb81663bfb68c259f03d3e784e9ab30224988a
Dmitri Konnov <lildkonnov.nnov@inbox.ru>
2023-04-06 01:02:09 +0400

    added methods generating matrices

--- src/Utils.java
+++ src/Utils.java
@@ -1,43 +1,42 @@
 public class Utils {
 
-    public static void createRandom2DMatrix(){
+    public static void generateRandom2DMatrix(){
         int length = (int) (Math.random() * 100);
         int width = (int) (Math.random() * 100);
-        createMatrix(length, width);
+        generateMatrix(length, width);
     }
 
-    public static void createRandom2DMatrix(int length, int width){
-        createMatrix(length, width);
+    public static void generateRandom2DMatrix(int length, int width){
+        generateMatrix(length, width);
     }
 
-    public static void createRandom2DMatrix(int size){
-        createMatrix(size, size);
+    public static void generateRandom2DMatrix(int size){
+        generateMatrix(size, size);
     }
 
-    public static void createRandomMatrices(int numberOfMatrices){
+    public static void generateRandomMatrices(int numberOfMatrices){
         for (int i = 0; i < numberOfMatrices; i++) {
-            createRandom2DMatrix();
+            generateRandom2DMatrix();
         }
     }
 
-    public static void createRandomMatrices(int numberOfMatrices, int length, int width){
+    public static void generateRandomMatrices(int numberOfMatrices, int length, int width){
         for (int i = 0; i < numberOfMatrices; i++) {
-            createRandom2DMatrix(length, width);
+            generateRandom2DMatrix(length, width);
         }
     }
 
-    public static void createRandomMatrices(int numberOfMatrices, int size){
+    public static void generateRandomMatrices(int numberOfMatrices, int size){
         for (int i = 0; i < numberOfMatrices; i++) {
-            createRandom2DMatrix(size);
+            generateRandom2DMatrix(size);
         }
     }
-    private static void createMatrix(int length, int width){
+    private static void generateMatrix(int length, int width){
         int[][] matrix = new int[length][width];
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 matrix[i][j] = (int) (Math.random() * 100);
             }
         }
-
     }
 }
