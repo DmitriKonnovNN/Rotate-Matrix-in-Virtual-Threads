16baeebb34432b28ff36134547c34b6e29cb5eab
Dmitri Konnov <lildkonnov.nnov@inbox.ru>
2023-04-17 01:48:53 +0400

    task executor added

---
+++ src/test/java/solutions/dmitrikonnov/AppTest.java
@@ -0,0 +1,20 @@
+package test.java.solutions.dmitrikonnov;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
