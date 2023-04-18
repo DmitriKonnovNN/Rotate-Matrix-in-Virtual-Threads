5ddc7498254434e1926a3d53a7f7a4fd715f8f25
Dmitri Konnov <lildkonnov.nnov@inbox.ru>
2023-04-18 14:11:19 +0400

    Bad bad decision made

--- src/main/java/solutions/dmitrikonnov/demo/RotateMatrix.java
+++ src/main/java/solutions/dmitrikonnov/demo/RotateMatrix.java
@@ -12,7 +12,7 @@ public interface RotateMatrix {
    void rotate180CompletableFutureWithExecutor(MatrixRotatorTask[] tasks, int numberOfThreads);
    void rotate180ParallelStream(MatrixRotatorTask[] tasks);
 
-   void rotate90VirtualThead(MatrixRotatorTask [] task);
+   void rotate90VirtualThead(MatrixRotatorTask [] task) throws InterruptedException;
 
    void rotate270Sequential(MatrixRotatorTask[] tasks);
    void rotate270CompletableFuture(MatrixRotatorTask[] tasks);
