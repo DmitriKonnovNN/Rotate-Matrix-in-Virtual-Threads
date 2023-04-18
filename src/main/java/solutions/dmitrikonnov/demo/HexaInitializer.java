24570ead4cff70f321fb5c1d1de03d6321fe8f21
Dmitri Konnov <lildkonnov.nnov@inbox.ru>
2023-04-18 20:59:30 +0400

    HexaSupplier with Generics replaced by HexaInitializer

--- src/main/java/solutions/dmitrikonnov/demo/HexaInitializer.java
+++ src/main/java/solutions/dmitrikonnov/demo/HexaInitializer.java
@@ -1,9 +1,6 @@
 package solutions.dmitrikonnov.demo;
-/**
- *
- * I could have called it HeptaSupplier, which effectively it is, but I didn't.
- * */
+
 @FunctionalInterface
-public interface HexaInitializer<Q,W,E,R,T,Y,U> {
-    U getInstance(Q q, W w, E e, R r, T t, Y y);
+public interface HexaInitializer<T> {
+    T initializeWithInt(int[][]matrix, int[][]newMatrix, int i, int j,int k ,int l);
 }
